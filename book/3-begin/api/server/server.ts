e844b31d150c392faacf1f6a6ab44ed79dbd25d8
Andrew Shrout <andrewshrout1@gmail.com>
2020-09-10 16:41:54 -0700

    book 4 problems

--- book/3-begin/api/server/server.ts
+++ book/3-begin/api/server/server.ts
@@ -5,6 +5,15 @@ const server = express();
 
 server.use(express.json());
 
+server.get('*', (_, res) => {
+  res.sendStatus(403);
+});
+
+server.get('/api/v1/public/get-user', (_, res) => {
+  console.log('API server got request from APP server');
+  res.json({ user: { email: 'team@builderbook.org' } });
+});
+
 server.listen(process.env.PORT_API, (err) => {
   if (err) {
     throw err;
